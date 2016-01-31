8ef380fd4f2d6d3ad374fec3e69e6ba99435c21d
SugarLight256 <nippen12@gmail.com>
2016-01-31 12:19:23 +0900

    FirstCommit

---
+++ Assets/Scripts/CameraMove_test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove_test : MonoBehaviour {
+    public float rotationSpeed=1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.position += new Vector3(0,-10 * Time.deltaTime,0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += new Vector3(0, 10 * Time.deltaTime, 0);
+        }
+    }
+}
